4d442281a9f4f6bf50265b5fd6c3117b86274448
doslahtm <59042712+doslahtm@users.noreply.github.com>
2021-09-06 01:05:11 +0900

    test production

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -69,6 +69,7 @@ const formatTime = (seconds) =>
   new Date(seconds * 1000).toISOString().substr(14, 5);
 
 const handleLoadedMetadata = () => {
+  console.log(video.duration);
   totalTime.innerText = formatTime(Math.floor(video.duration));
   timeline.max = Math.floor(video.duration);
 };
